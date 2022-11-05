8c5bcf74bd35bf1f16354e04285f58b47da1e781
Chris <gorgorchrislo@gmail.com>
2022-11-05 15:43:52 -0400

    benchmark

---
+++ main.js
@@ -0,0 +1,19 @@
+gsap.from("#mercury", { scrollTrigger: {scrub:true}, x: 300 });
+gsap.from("#venus", { scrollTrigger: {scrub:true}, x: -200 });
+gsap.from("#earth", { scrollTrigger: {scrub:true}, x: 300 });
+gsap.from("#mars", { scrollTrigger: {scrub:true}, x: -200 });
+gsap.from("#jupiter", { scrollTrigger: {scrub:true}, x: 300 });
+gsap.from("#saturn", { scrollTrigger: {scrub:true}, x: -200 });
+gsap.from("#uranus", { scrollTrigger: {scrub:true}, x: 300 });
+gsap.from("#neptune", { scrollTrigger: {scrub:true}, x: -200 });
+
+// var yourImg = document.getElementById('saturn');
+// if(yourImg && yourImg.style) {
+//     yourImg.style.height = '100px';
+//     yourImg.style.width = '200px';
+// }
+// var planets = document.getElementsByClassName();
+// function enlarge(image) {
+//     image.style.height += 5;
+//     image.style.weidth += 5;
+// }
